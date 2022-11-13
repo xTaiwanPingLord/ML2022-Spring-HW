8b49eaaa0a8a185a0d22053197ba9cfbe15cbaec
xTaiwanPingLord <tojimmy0917@gmail.com>
2022-11-13 09:06:58 +0800

    20221113

--- HW3/hw3.py
+++ HW3/hw3.py
@@ -42,11 +42,11 @@ _exp_name = "sample"
 ##### Functions ##############################################################
 # fix seed for reproducibility
 def same_seeds(seed):
+    np.random.seed(seed)
     torch.manual_seed(seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
-    np.random.seed(seed)
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
 
